b7b4e6920fd8578cd92f190946afdb121e9146c2
Pranav Bansod <pranav.bansod.14@gmail.com>
2018-01-12 14:52:18 +0530

    App doesnt allow non-user to view pages other than login page.

--- app.js
+++ app.js
@@ -5,17 +5,6 @@ let registered_users = [{userName:'prateikp'}, {userName:'pranavb'}];
 
 let app = WebApp.create();
 
-const logRequest = (req,res)=>{
-  let text = ['------------------------------',
-  `${timeStamp()}`,
-  `${req.method} ${req.url}`,
-  `HEADERS=> ${toString(req.headers)}`,
-  `COOKIES=> ${toString(req.cookies)}`,
-  `BODY=> ${toString(req.body)}`,''].join('\n');
-  fs.appendFile('request.log',text,()=>{});
-  // console.log(`${req.method} ${req.url}`);
-};
-
 const getContentType = function(filename) {
   let extension = filename.slice(filename.lastIndexOf('.'));
   let contentType = {
@@ -32,6 +21,68 @@ const getContentType = function(filename) {
 
 let toString = (obj)=>JSON.stringify(obj,null,2);
 
+const logRequest = (req,res)=>{
+  let text = ['------------------------------',
+  `${timeStamp()}`,
+  `${req.method} ${req.url}`,
+  `HEADERS=> ${toString(req.headers)}`,
+  `COOKIES=> ${toString(req.cookies)}`,
+  `BODY=> ${toString(req.body)}`,''].join('\n');
+  fs.appendFile('request.log',text,()=>{});
+  // console.log(`${req.method} ${req.url}`);
+};
+
+let loadUser = (req,res)=>{
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u=>u.sessionid==sessionid);
+  if(sessionid && user){
+    req.user = user;
+  }
+};
+
+let redirectLoggedInUserToHome = (req,res)=>{
+  if(req.urlIsOneOf(['/','/login.html']) && req.user) {
+    res.statusCode = 302;
+    res.setHeader('location','/homePage.html');
+    res.end();
+  }
+};
+
+let redirectLoggedOutUserToLogin = (req,res)=>{
+  let urlAllowedForOnlyLoggedInUsers = ['/', '/homePage.html', '/logout', '/toDo.html', '/item.html'];
+  if(req.urlIsOneOf(urlAllowedForOnlyLoggedInUsers) && !req.user) {
+    res.statusCode = 302;
+    res.setHeader('location','/login.html');
+    res.end();
+  }
+};
+
+app.addPreProcessor(redirectLoggedInUserToHome);
+app.addPreProcessor(redirectLoggedOutUserToLogin);
+app.addPreProcessor(logRequest);
+app.addPreProcessor(loadUser);
+
+
+// ======================================================================
+
+app.post('/login.html',(req,res)=>{
+  let user = registered_users.find(u=>u.userName==req.body.userName);
+  res.setHeader('Set-Cookie',`logInFailed=false`);
+  if(!user) {
+    res.setHeader('Set-Cookie',`logInFailed=true`);
+    res.redirect('/login.html');
+    return;
+  }
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.setHeader('location','/homePage.html');
+  res.end()
+});
+
+
+// ======================================================================
+
 const processForFileFound = function(req,res,filename) {
   let contentType = getContentType(filename);
   res.setHeader('Content-Type',contentType);
@@ -44,11 +95,16 @@ const processForPageNotFound = function(req,res) {
   res.write("Page Not Found")
 }
 
+const redirectToHomePage = function(req,res) {
+  res.statusCode = 302;
+  res.setHeader('location','/homePage.html');
+  res.end()
+}
 
 const fileServer = function(req,res) {
   let filename = "./public" + req.url;
   if(filename=="./public/") {
-    filename = "./public/index.html";
+    redirectToHomePage(req,res);
   }
   if(fs.existsSync(filename)) {
     processForFileFound(req,res,filename);
@@ -58,7 +114,6 @@ const fileServer = function(req,res) {
   res.end();
 };
 
-app.addPreProcessor(logRequest);
 app.addPostProcessor(fileServer);
 
 module.exports = app;
--- tests/appTest.js
+++ tests/appTest.js
@@ -5,7 +5,7 @@ process.env.COMMENT_STORE = "./testStore.json";
 let app = require('../app.js');
 let th = require('./testHelper.js');
 
-describe('app',()=>{
+describe('app without Login',()=>{
   describe('GET /bad',()=>{
     it('responds with 404',(done)=>{
       request(app,{method:'GET',url:'/bad'},(res)=>{
@@ -14,4 +14,56 @@ describe('app',()=>{
       })
     })
   })
+
+  describe('GET /',()=>{
+    it('redirects to login.html',done=>{
+      request(app,{method:'GET',url:'/'},(res)=>{
+        th.should_be_redirected_to(res,'/login.html');
+        assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
+
+  describe('GET /homePage.html',()=>{
+    it('redirects to login.html',done=>{
+      request(app,{method:'GET',url:'/homePage.html'},res=>{
+        th.should_be_redirected_to(res,'/login.html');
+        assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
+
+  describe('GET /toDo.html',()=>{
+    it('redirects to login.html',done=>{
+      request(app,{method:'GET',url:'/toDo.html'},res=>{
+        th.should_be_redirected_to(res,'/login.html');
+        assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
+
+  describe('GET /item.html',()=>{
+    it('redirects to login.html',done=>{
+      request(app,{method:'GET',url:'/item.html'},res=>{
+        th.should_be_redirected_to(res,'/login.html');
+        assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
+
+  describe('GET /item.html',()=>{
+    it('serves login.html',done=>{
+      request(app,{method:'GET',url:'/login.html'},res=>{
+        th.status_is_ok(res);
+        th.content_type_is(res,'text/html');
+        th.body_contains(res,'Login Form')
+        done();
+      })
+    })
+  })
+
 })
